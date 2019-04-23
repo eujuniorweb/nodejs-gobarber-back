b41c139bc52ad363ada52ec6ae263bf84e47b00f
Juninho Silva <jsdeveloperweb@gmail.com>
2019-04-22 23:34:14 -0300

    Configurando o servidor

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+    midlewares()
+    views()
+    routes()
+  }
+
+  midlewares () {
+    this.express.use(express.urlencoded({ extended: false }))
+  }
+  views () {
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+      autoescape: true,
+      express: this.express,
+      watch: this.isDev
+    })
+    this.express.set('view engine', 'njk')
+  }
+  routes () {}
+}
+module.exports = new App().express
