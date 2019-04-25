af1ffd3e8bdd91ed12ca3ffdc622b7fb58208834
Juninho Silva <jsdeveloperweb@gmail.com>
2019-04-24 21:06:30 -0300

    Middleware de auth e guest

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middleware/auth')
+const guestMiddleware = require('./app/middleware/guest')
 
-routes.get('/', SessionController.create)
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
+routes.use('/app', authMiddleware)
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
   return res.render('dashboard')
