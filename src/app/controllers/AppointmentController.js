af27b26a8fc64417d0bec05a2f0ea73bb83756a1
jsdeveloper <jros2.silva@gmail.com>
2019-08-22 17:49:32 -0300

    cancelando agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -86,6 +86,22 @@ class AppointmentController {
     return res.json(appointment);
   }
 
-  async delete(req, res) {}
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+    if (appointment.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: 'You not have permission to cancel this appointment' });
+    }
+    const dateWithSub = subHours(appointment.date, 2);
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointment two hours in advance',
+      });
+    }
+    appointment.canceled_at = new Date();
+    await appointment.save();
+    return res.json(appointment);
+  }
 }
 export default new AppointmentController();
