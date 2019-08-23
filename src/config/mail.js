88203f51092c8d273e3937f9d2c83a4914bc4808
jsdeveloper <jros2.silva@gmail.com>
2019-08-22 23:12:00 -0300

    confgurando e testando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '050d56b8860f5c',
+    pass: '15a29e29195a0e',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
